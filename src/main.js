46cd2429e771b0293f0e7b3d3bdfdca2e9a7c8a5
muyi <7637291+muyi223@user.noreply.gitee.com>
2020-12-09 20:11:41 +0800

    完成了登錄功能

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
-Vue.config.productionTip = false
+//导入全局样式表
 
+import './assets/css/global.css'
+
+import axios from 'axios'
+axios.defaults.baseURL='https://www.liulongbin.top:8888/api/private/v1/'
+
+// axios.default.baseURL ='http://timemeetyou.com:8889/api/private/v1/'
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App)
